c6d7f06fe2cd699b4fab38a16aac188d3e39c89d
jongwon0801 <129039041+jongwon0801@users.noreply.github.com>
2025-04-14 12:05:34 +0900

    Delete source/sshstop.sh

--- source/sshstop.sh
+++
@@ -1,9 +0,0 @@
-
-#!/bin/bash
-
-if [[ -e /tmp/reverse_ssh.pid ]]; then   # If the file do not exists, then the
-    kill `cat /tmp/reverse_ssh.pid`      #+the process is not running. Useless
-    rm /tmp/reverse_ssh.pid              #+trying to kill it.
-else
-    echo "reverse ssh is not running"
-fi
