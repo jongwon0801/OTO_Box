f3db0856c30676cb9178d7bcbc9f33f07198e97a
jongwon0801 <129039041+jongwon0801@users.noreply.github.com>
2025-04-16 16:20:01 +0900

    Create wsgi.py

---
+++ guide/정은치엘로B/source/wsgi.py
@@ -0,0 +1,18 @@
+# /home/pi/Workspace/appleapp/appleapp/wsgi.py
+
+"""
+WSGI config for appleapp project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "appleapp.settings")
+
+application = get_wsgi_application()
