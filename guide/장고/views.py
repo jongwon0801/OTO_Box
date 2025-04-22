7f1c1dd862d3cbf617562145e2b6b2fefb940093
jongwon0801 <129039041+jongwon0801@users.noreply.github.com>
2025-04-22 11:28:24 +0900

    Update views.py

--- guide/장고/views.py
+++ guide/장고/views.py
@@ -1,5 +1,7 @@
 # -*- coding: UTF-8 -*-
 
+# /home/pi/Workspace/newapp/appleapp/views.py
+
 from django.http import HttpResponse
 #from django.shortcuts import render_to_response
 from django.shortcuts import render
