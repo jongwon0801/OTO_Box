7f5ade7511e51542c05915da5a5d21f6d0d9415a
jongwon0801 <129039041+jongwon0801@users.noreply.github.com>
2025-04-22 11:29:26 +0900

    Create urls.py

---
+++ guide/장고/urls.py
@@ -0,0 +1,50 @@
+# /home/pi/Workspace/newapp/appleapp/urls.py
+
+"""appleapp URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.10/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.conf.urls import url, include
+    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+"""
+from django.conf.urls import url
+from django.contrib import admin
+from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+from django.conf.urls import url, include
+urlpatterns = [
+    #url(r'^logsCams$', views.LogsKivy, name='LogsCams'),
+    #url(r'^logsCams/(?P<dirname>[-\w.]+)/$', views.LogsCamsDir, name='LogsCamsDir'),
+    #url(r'^logsCams/(?P<dirname>[-\w.]+)/(?P<filename>[-\w.]+)$', views.LogsCamsFile, name='LogsCamsFile'),
+
+    url(r'^logsKivy$', views.LogsKivy, name='LogsKivy'),
+    url(r'^logsKivy/(?P<filename>[-\w.]+)/$', views.LogsKivyFile, name='LogsKivyFile'),
+    url(r'logsDjango$', views.LogsDjango, name='LogsDjango'),
+    url(r'logsDjango/(?P<filename>[-\w.]+)/$', views.LogsDjangoFile, name='LogsDjangoFile'),
+    url(r'^logsPush', views.LogsPush, name='LogsPush'),
+    url(r'^$', views.index,name='index'),
+    url(r'^whois', views.whois, name='whois'),
+    #url(r'^admin/', admin.site.urls),
+    url(r'^v1/', include('applebox.urls')),
+    url(r'^readme/', views.readme, name='readme'),
+    url(r'^setup/', views.setup, name='setup'),
+    url(r'^test/', views.test, name='test'),
+    url(r'^o2obox', views.api, name='api'),
+    url(r'^django/', views.django, name='django'),
+    url(r'^newinstall/', views.newinstall, name='newinstall'),
+    url(r'^raspberrypi/', views.raspberrypi, name='raspberrypi'),
+    url(r'^DeviceRegister/', views.DeviceRegister, name='DeviceRegister'),
+    url(r'^WifiScan/', views.WifiScan, name='WifiScan'),
+    url(r'^TT/', views.TT, name='TT'),
+    url(r'^OpenDoor/(?P<controller>[0-9]+)$', views.OpenDoor, name='OpenDoor'),
+]# + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
