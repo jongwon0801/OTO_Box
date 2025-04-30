e8b19832a0cf6cbe2528d23e1b2f17f1fd9520a4
jongwon0801 <129039041+jongwon0801@users.noreply.github.com>
2025-04-30 12:31:27 +0900

    Create views.py

---
+++ source/원격접속/views.py
@@ -0,0 +1,33 @@
+# /home/pi/Workspace/newapp/applebox/views.py
+# appleapp에서 applebox를 import
+
+```less
+from django.http import JsonResponse
+import subprocess
+
+def AutosshStart(request, outport, inport):
+    # 문자열로 변환 필수!
+    command = ['/home/pi/reversessh.sh', 'start', str(outport), str(inport)]
+
+    process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    stdout, stderr = process.communicate()
+
+    print(stdout.decode())
+    if stderr:
+        print("ERROR:", stderr.decode())
+
+    return JsonResponse({'success': True})
+
+
+def AutosshStop(request):
+    command = ['/home/pi/reversessh.sh', 'stop']
+    process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    stdout, stderr = process.communicate()
+
+    print(stdout.decode())
+    if stderr:
+        print("ERROR:", stderr.decode())
+
+    return JsonResponse({'success': True})
+
+```
